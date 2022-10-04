1cda78376662c5db8a4bd053eb26ef22d85b4614
Pauline Cha <pacha@davidson.edu>
2022-10-04 10:32:49 -0400

    reactive portion for lg,md,sm (3 breakpoints)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,9 @@
 
 /* javascript */
+
+function resetViewbox(e){
+   box.setAttribute("viewBox","0 0 "+e[0].contentBoxSize[0].inlineSize+" "+e[0].contentBoxSize[0].blockSize)
+   //console.log("New viewBox: ", box.getAttribute("viewBox"))
+}
+
+new ResizeObserver(resetViewbox).observe(svgContent)
